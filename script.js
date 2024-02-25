96af85cdac7d736b8fcc3ab4d15752f2cdcefe2e
aiyyer <mitchrc2000@gmail.com>
2024-02-24 17:21:31 -0800

    no more partial images

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
-// window.onload = function(){
-// 	document.addEventListener(`click`, moveAround);
-// 	document.body.style.background = `url("./entrance.png") no-repeat center center fixed`;
-// 	document.body.style.backgroundSize = `contain`;
-// }
+let image;
+window.onload = function(){
+	image = document.getElementById('image');
+	image.style.width = `800px`;
+	image.style.background = `no-repeat url(./foyer/entrance.png)`;
+	image.style.height = `500px`;
+	image.addEventListener('click', turn);
+}
 
-// function moveAround(event) {
-// 	if(event.clientX > event.target.getBoundingClientRect().right - 100) {
-// 		toRightRoom();
-// 	} else if(event.clientX < event.target.getBoundingClientRect().left + 100) {
-// 		toLeftRoom();
-// 	}
-// }
+function turn(event) {
+	let imageBounds = image.getBoundingClientRect();
+	if(!(event.clientX > imageBounds.left && event.clientX < imageBounds.left + imageBounds.width/16 || event.clientX < imageBounds.right && event.clientX > imageBounds.right - imageBounds.width/16)) {
+		return false;
+	}
+	let currentDirection = image.style.background.split('.').reverse()[1].split(`/`).reverse()[0];
+	let sourceImage = ["library", "workshop", "study", "entrance"];
+	if(event.clientX < imageBounds.right && event.clientX > imageBounds.right - imageBounds.width/16) {
+		sourceImage = ["study", "entrance", "library", "workshop"];
+	}
+	switch(currentDirection) {
+	case "entrance":
+		setBackground(sourceImage[0]);
+		break;
+	case "library":
+		setBackground(sourceImage[1]);
+		break;
+	case "workshop":
+		setBackground(sourceImage[2]);
+		break;
+	case "study":
+		setBackground(sourceImage[3]);
+		break;
+	default:
+	}
+}
 
-// function toLeftRoom() {
-// 	if(document.body.style.background.includes(`entrance`)) {
-// 		document.body.style.background = `url("./library.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;
-// 	} else if(document.body.style.background.includes(`library`)) {
-// 		document.body.style.background = `url("./workshop.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;	
-// 	} else if(document.body.style.background.includes(`study`)) {
-// 		document.body.style.background = `url("./entrance.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;	
-// 	} else {
-// 		document.body.style.background = `url("./study.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;	
-// 	}
-// }
-
-// function toRightRoom() {
-// 	if(document.body.style.background.includes(`entrance`)) {
-// 		document.body.style.background = `url("./study.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;
-// 	} else if(document.body.style.background.includes(`library`)) {
-// 		document.body.style.background = `url("./entrance.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;	
-// 	} else if(document.body.style.background.includes(`study`)) {
-// 		document.body.style.background = `url("./workshop.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;	
-// 	} else {
-// 		document.body.style.background = `url("./library.png") no-repeat center center fixed`;
-// 		document.body.style.backgroundSize = `contain`;	
-// 	}
-// }
+function setBackground(filename) {
+	image.style.background = `no-repeat url(./foyer/${filename}.png)`;
+}
